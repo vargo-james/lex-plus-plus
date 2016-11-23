25605819c9744657fddf01d9db6bcdb985e0662e
James Vargo <vargo.james@gmail.com>
2016-11-23 12:16:39 -0800

    Wrote a test for singleton matchers.

--- matcher_test/atomic_matcher_test.cpp
+++ matcher_test/atomic_matcher_test.cpp
@@ -13,12 +13,14 @@ using namespace lex;
 // A singleton_matcher_transition object does not need to change its state
 // or even carry any state other than the constant char with which it was
 // initialized. 
+//
+// This also tests a copy.
 int singleton_matcher_transition_test(std::ostream& os) {
   int error_count {0};
-
-  std::string smtt {"single_matcher_transition_test: "};
+  const std::string smtt {"single_matcher_transition_test: "};
 
   singleton_matcher_transition<char> trans('a');
+  auto copy = trans;
   if (trans.update('a') != match_state::FINAL_MATCH) {
     test_log(os, smtt);
     ++error_count;
@@ -39,12 +41,60 @@ int singleton_matcher_transition_test(std::ostream& os) {
     ++error_count;
   }
 
+  if (copy.update('a') != match_state::FINAL_MATCH) {
+    test_log(os, smtt);
+    ++error_count;
+  }
+
+  if (copy.initialize() != match_state::UNDECIDED) {
+    test_log(os, smtt);
+    ++error_count;
+  }
+
+  if (copy.update('x') != match_state::MISMATCH) {
+    test_log(os, smtt);
+    ++error_count;
+  }
+  return error_count;
+}
+
+int singleton_matcher_test(std::ostream& os) {
+  int error_count {0};
+  const std::string smt {"single_matcher_test: "};
+
+  auto matcher = singleton_matcher('b');
+  if (matcher.state() != match_state::UNDECIDED) {
+    test_log(os, smt);
+    ++error_count;
+  }
+  matcher.update('b');
+  if (matcher.state() != match_state::FINAL_MATCH) {
+    test_log(os, smt);
+    ++error_count;
+  }
+  matcher.update('b');
+  if (matcher.state() != match_state::MISMATCH) {
+    test_log(os, smt);
+    ++error_count;
+  }
+  matcher.initialize();
+  if (matcher.state() != match_state::UNDECIDED) {
+    test_log(os, smt);
+    ++error_count;
+  }
+  matcher.update(';');
+  if (matcher.state() != match_state::MISMATCH) {
+    test_log(os, smt);
+    ++error_count;
+  }
+
   return error_count;
 }
 
 int atomic_matcher_test(std::ostream& os) {
   test_suite composition {
-    singleton_matcher_transition_test
+    singleton_matcher_transition_test,
+    singleton_matcher_test
   };
   return composition(os);
 }
