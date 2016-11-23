c4a0fa8d73048c5f3393d17fa60e144df76553cd
James Vargo <vargo.james@gmail.com>
2016-11-22 16:59:46 -0800

    Added/incorporated blank atomic_matcher_test.
    The test needs to be filled in. But it is incorporated into the
    module and library tests.

--- matcher_test/matcher_module_test.h
+++ matcher_test/matcher_module_test.h
@@ -3,12 +3,14 @@
 
 #include "matcher_transition_test.h"
 #include "matcher_test.h"
+#include "atomic_matcher_test.h"
 #include "test_machinery.h"
 
 inline int matcher_module_test(std::ostream& os) {
   test_suite module_test {
     matcher_test, 
-    matcher_transition_test
+    matcher_transition_test,
+    atomic_matcher_test
   };
   return module_test(os);
 }
